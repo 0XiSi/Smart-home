63073529e8b1378b20ac643b2605a4b7beeaa7cf
Administrator <mozirimohhamad1@gmail.com>
2024-03-18 18:23:39 +0330

    a

--- smart_home/client_mqtt.py
+++ smart_home/client_mqtt.py
@@ -1,23 +1,32 @@
 import paho.mqtt.client as mqtt
 
-
-def on_connect(mqtt_client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print('Connected successfully')
-        mqtt_client.subscribe('django/mqtt')
+        client.subscribe('django/mqtt')
     else:
         print('Bad connection. Code:', rc)
 
-
-def on_message(mqtt_client, userdata, msg):
+def on_message(client, userdata, msg):
     print(f'Received message on topic: {msg.topic} with payload: {msg.payload}')
 
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.username_pw_set('', '')
-    client.connect(
-        host='t0c2aec5.ala.asia-southeast1.emqxsl.com',
-        port=8883,
-        keepalive=60
-    )
+# Create a client instance
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+
+# Assign callback functions
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Set username and password if required
+client.username_pw_set('', '')
+
+# Connect to MQTT broker
+client.connect(
+    host='85a4979f4e39416e9fabd326a49b02a6.s1.eu.hivemq.cloud',
+    port=8883,
+    keepalive=60
+)
+
+# Start the MQTT loop
+client.loop_forever()
