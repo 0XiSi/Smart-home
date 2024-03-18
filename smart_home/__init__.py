068596cba7cb662f19655e6070e3aace0a3bcaf8
Administrator <mozirimohhamad1@gmail.com>
2024-03-18 18:14:57 +0330

    a

--- smart_home/__init__.py
+++
@@ -1,2 +0,0 @@
-from client_mqtt import mqtt
-mqtt.Client.loop_start()
--- smart_home/client_mqtt.py
+++ smart_home/client_mqtt.py
@@ -1,7 +1,5 @@
 import paho.mqtt.client as mqtt
 
-from smart_home import settings
-
 
 def on_connect(mqtt_client, userdata, flags, rc):
     if rc == 0:
@@ -17,9 +15,9 @@ def on_message(mqtt_client, userdata, msg):
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-    client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
+    client.username_pw_set('', '')
     client.connect(
-        host=settings.MQTT_SERVER,
-        port=settings.MQTT_PORT,
-        keepalive=settings.MQTT_KEEPALIVE
+        host='t0c2aec5.ala.asia-southeast1.emqxsl.com',
+        port=8883,
+        keepalive=60
     )
