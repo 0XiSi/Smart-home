582f61c04da1a774d6f43e6f4e359094a811aea4
Administrator <mozirimohhamad1@gmail.com>
2024-03-18 20:02:27 +0330

    a

--- smart_home/client_mqtt2.py
+++ smart_home/client_mqtt2.py
@@ -1,27 +1,26 @@
-import time
+import threading
 import paho.mqtt.client as paho
 from paho import mqtt
-
 
 # setting callbacks for different events to see if it works, print the message etc.
 def on_connect(client, userdata, flags, rc, properties=None):
     print("CONNACK received with code %s." % rc)
 
-
 # with this callback you can see if your publish was successful
 def on_publish_v5(client, userdata, mid, reasonCode, properties):
     print("mid: " + str(mid))
-
 
 # print which topic was subscribed to
 def on_subscribe(client, userdata, mid, granted_qos, properties=None):
     print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
-
 # print message, useful for checking if it was successful
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
+# function to run the MQTT client loop in a separate thread
+def mqtt_loop():
+    client.loop_forever()
 
 # using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
 # userdata is user defined data of any type, updated by user_data_set()
@@ -45,9 +44,11 @@ client.on_publish_v5 = on_publish_v5
 # subscribe to all topics of encyclopedia by using the wildcard "#"
 client.subscribe("b", qos=1)
 
-# a single publish, this can also be done in loops, etc.
-client.publish("b", payload="hot", qos=1)
+# start the MQTT client loop in a separate thread
+mqtt_thread = threading.Thread(target=mqtt_loop)
+mqtt_thread.start()
 
-# loop_forever for simplicity, here you need to stop the loop manually
-# you can also use loop_start and loop_stop
-client.loop_forever()
+# loop for publishing messages
+while True:
+    message = input("Enter a message to publish: ")
+    client.publish("b", payload=message, qos=1)
